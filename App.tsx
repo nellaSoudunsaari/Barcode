89abe756144c72d9d4b6a2a04327231f2729650d
Nella <nellasoudunsaari@gmail.com>
2026-02-12 12:48:18 +0200

    barcode reader done

--- App.tsx
+++ App.tsx
@@ -1,11 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
+import { CameraView, CameraType, useCameraPermissions } from 'expo-camera';
+import { useState } from 'react';
 
 export default function App() {
+  const [permission, requestPermission] = useCameraPermissions();
+  const [ scannedBarcode, setScannedBarcode ] = useState("no barcode scanned");
+
+  if(!permission){
+    return <View/>;
+  }
+
+  if(!permission.granted){
+    return(
+      <View style={styles.container}>
+        <Text style={styles.message}>App needs permission to show the camera.</Text>
+        <Button onPress={requestPermission} title='Grant Permission'/>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <CameraView 
+        style={styles.camera} 
+        facing='back'
+        barcodeScannerSettings={
+          {
+            barcodeTypes: ['ean13', 'code128', 'code39', 'code93', 'ean8', 'codabar']
+          }
+        }
+        onBarcodeScanned={
+          ({ data }) => {
+            setScannedBarcode(data);
+          }
+        }
+      />
+      <View style={styles.resultContainer}>
+        <Text style={styles.text}>{scannedBarcode}</Text>
+      </View>
     </View>
   );
 }
@@ -14,7 +47,32 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
+  message: {
+    textAlign: 'center',
+    paddingBottom: 10,
+  },
+  camera: {
+    flex: 1,
+  },
+  resultContainer: {
+    position: 'absolute',
+    bottom: 64,
+    padding: 20,
+    flexDirection: 'row',
+    backgroundColor: 'rgba(0, 0, 0, 0.7)',
+    width: '100%',
+    paddingHorizontal: 64,
+    justifyContent: 'center'
+  },
+  button: {
+    flex: 1,
+    alignItems: 'center'
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white'
+  }
 });
